6aa9e710f139c8396e1999b941d7dc7c48e34436
Nick Seguin <5571025+nseguin42@users.noreply.github.com>
2023-12-03 03:32:37 -0600

    add day 3 part 2

--- NSeguin.Dev.AdventOfCode/Solutions/Year2023/Day3.cs
+++ NSeguin.Dev.AdventOfCode/Solutions/Year2023/Day3.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace NSeguin.Dev.AdventOfCode.Solutions.Year2023;
 
 public partial class Day3() : AdventOfCodeSolution(2023, 3)
@@ -13,54 +15,25 @@ public partial class Day3() : AdventOfCodeSolution(2023, 3)
         string input,
         CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return ValueTask.FromResult(Day2.GetTotal(input).ToString());
     }
 
-    internal static class Day1
+    public class AdjacentToComparer : IEqualityComparer<Schematic.CharInfo>
     {
-        public static int GetTotal(string input)
+        public static AdjacentToComparer Default { get; } = new();
+
+        public bool Equals(Schematic.CharInfo x, Schematic.CharInfo y)
         {
-            var schematic = new Schematic(input);
-            var initialCharsOfNumbers = schematic.Chars.SelectMany(x => x)
-                .Where(schematic.IsStartOfNumber)
-                .ToList();
+            return Math.Abs(x.X - y.X) <= 1 && Math.Abs(x.Y - y.Y) <= 1;
+        }
 
-            var numberSpansWhereAnyIsAdjacentToSymbol = initialCharsOfNumbers.Select(
-                    c =>
-                    {
-                        int startOfNumber = c.X;
-                        (int x, int y) = (c.X, c.Y);
-                        bool isAdjacentToSymbol = false;
-                        do
-                        {
-                            isAdjacentToSymbol |= schematic.IsAdjacentToSymbolAt(x, y);
-                            x++;
-                        }
-                        while (x < schematic.Chars[y].Length && schematic.IsNumberAt(x, y));
-
-                        if (isAdjacentToSymbol)
-                        {
-                            return schematic.Chars[y][startOfNumber..x]
-                                .Select(x => x.Char)
-                                .ToArray();
-                        }
-
-                        return null;
-                    })
-                .Where(x => x is not null)
-                .ToList();
-
-            var numbersAdjacentToSymbol = numberSpansWhereAnyIsAdjacentToSymbol
-                .Select(x => int.Parse(x))
-                .ToList();
-
-            var total = numbersAdjacentToSymbol.Sum();
-            return total;
+        public int GetHashCode(Schematic.CharInfo obj)
+        {
+            return 0;
         }
     }
 
     public partial class Schematic
-
     {
         public Schematic(string input)
         {
@@ -72,21 +45,14 @@ public partial class Day3() : AdventOfCodeSolution(2023, 3)
                         .ToArray())
                 .ToArray();
 
-            PositionsAdjacentToSymbols = Chars
-                .SelectMany((row, y) => row.Select((c, x) => (c, x, y)))
-                .Where(t => t.c.IsSymbol)
-                .SelectMany(
-                    t => new[]
-                    {
-                        (t.x - 1, t.y - 1), (t.x, t.y - 1), (t.x + 1, t.y - 1), (t.x - 1, t.y),
-                        (t.x + 1, t.y), (t.x - 1, t.y + 1), (t.x, t.y + 1), (t.x + 1, t.y + 1),
-                    })
-                .ToHashSet();
+            PositionsAdjacentToSymbols = Chars.SelectMany(x => x)
+                .Where(c => c.IsSymbol)
+                .ToHashSet(AdjacentToComparer.Default);
         }
 
         public CharInfo[][] Chars { get; }
 
-        public HashSet<(int X, int Y)> PositionsAdjacentToSymbols { get; }
+        public HashSet<CharInfo> PositionsAdjacentToSymbols { get; }
 
         public CharInfo GetCharAt(int x, int y)
         {
@@ -108,19 +74,125 @@ public partial class Day3() : AdventOfCodeSolution(2023, 3)
             return GetCharAt(x, y).IsNumber;
         }
 
-        public bool IsAdjacentToSymbolAt(int x, int y)
+        public bool IsAdjacentToSymbol(CharInfo c, out CharInfo adjacentToCharInfo)
         {
-            return PositionsAdjacentToSymbols.Contains((x, y));
+            return PositionsAdjacentToSymbols.TryGetValue(c, out adjacentToCharInfo);
         }
 
-        public class CharInfo(char c, int x, int y)
+        public readonly struct CharInfo(char c, int x, int y)
         {
             public char Char { get; } = c;
-            public int X { get; set; } = x;
-            public int Y { get; set; } = y;
+            public int X { get; } = x;
+            public int Y { get; } = y;
             public bool IsNumber => char.IsDigit(Char);
 
             public bool IsSymbol => !IsNumber && Char != '.';
+            public bool IsGear => Char == '*';
+        }
+    }
+
+    internal static class Day1
+    {
+        public static int GetTotal(string input)
+        {
+            var schematic = new Schematic(input);
+            var initialCharsOfNumbers = schematic.Chars.SelectMany(x => x)
+                .Where(schematic.IsStartOfNumber)
+                .ToList();
+
+            var numbersAdjacentToSymbol = initialCharsOfNumbers.Select(
+                    c =>
+                    {
+                        int startOfNumber = c.X;
+                        (int x, int y) = (c.X, c.Y);
+                        List<Schematic.CharInfo> adjacentTo = [];
+                        do
+                        {
+                            var charInfo = schematic.GetCharAt(x, y);
+                            if (schematic.IsAdjacentToSymbol(charInfo, out var adjacentToCharInfo))
+                            {
+                                adjacentTo.Add(adjacentToCharInfo);
+                            }
+
+                            x++;
+                        }
+                        while (x < schematic.Chars[y].Length && schematic.IsNumberAt(x, y));
+
+                        if (adjacentTo.Count > 0)
+                        {
+                            var numberSpan = schematic.Chars[y][startOfNumber..x]
+                                .Select(x => x.Char)
+                                .ToArray();
+
+                            var number = int.Parse(numberSpan);
+                            return (Number: number, AdjacentTo: adjacentTo);
+                        }
+
+                        return default;
+                    })
+                .Where(x => x != default)
+                .ToList();
+
+            var total = numbersAdjacentToSymbol.Sum(x => x.Number);
+            return total;
+        }
+    }
+
+    internal static class Day2
+    {
+        public static int GetTotal(string input)
+        {
+            var schematic = new Schematic(input);
+            var initialCharsOfNumbers = schematic.Chars.SelectMany(x => x)
+                .Where(schematic.IsStartOfNumber)
+                .ToList();
+
+            var numbersAdjacentToSymbol = initialCharsOfNumbers.Select(
+                    c =>
+                    {
+                        int startOfNumber = c.X;
+                        (int x, int y) = (c.X, c.Y);
+                        List<Schematic.CharInfo> adjacentTo = [];
+                        do
+                        {
+                            var charInfo = schematic.GetCharAt(x, y);
+                            if (schematic.IsAdjacentToSymbol(charInfo, out var adjacentToCharInfo))
+                            {
+                                adjacentTo.Add(adjacentToCharInfo);
+                            }
+
+                            x++;
+                        }
+                        while (x < schematic.Chars[y].Length && schematic.IsNumberAt(x, y));
+
+                        var adjacentToGears = adjacentTo.Where(c => c.IsGear)
+                            .DistinctBy(c => (c.X, c.Y))
+                            .ToList();
+
+                        if (adjacentToGears.Count > 0)
+                        {
+                            var numberSpan = schematic.Chars[y][startOfNumber..x]
+                                .Select(x => x.Char)
+                                .ToArray();
+
+                            var number = int.Parse(numberSpan);
+                            return (Number: number, AdjacentToGears: adjacentToGears);
+                        }
+
+                        return default;
+                    })
+                .Where(x => x != default)
+                .ToList();
+
+            var numbersGroupedByGear = numbersAdjacentToSymbol
+                .SelectMany(x => x.AdjacentToGears.Select(g => (Number: x.Number, Gear: g)))
+                .GroupBy(x => x.Gear)
+                .Where(x => x.Count() == 2)
+                .Select(x => (First: x.First(), Second: x.Last()))
+                .ToList();
+
+            var total = numbersGroupedByGear.Sum(x => x.First.Number * x.Second.Number);
+            return total;
         }
     }
 }
